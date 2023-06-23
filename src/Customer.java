1cd61cec7126cbd2d7ef03374a12bfc5d48e688e
MertRam35 <mert.ramadan35@gmail.com>
2023-06-23 18:58:04 +0300

    Test

--- src/Customer.java
+++ src/Customer.java
@@ -2,14 +2,27 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 public class Customer {
-
+    private UUID id;
     private String userName;
     private String email;
     private List<Address> address;
 
+    public Customer(UUID id, String userName, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public Customer(UUID id, String userName, String email, List<Address> address) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.util.UUID;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-
-        createCustomer();
+Customer customer = new Customer(UUID.randomUUID(),"mert","mert@mert.com");
+        System.out.println("customer.getId() = " + customer.getUserName());
+  /*      createCustomer();
         createProduct();
         createCategory();
         createBalance();
         createDiscount();
 
-
+*/
 
 
 
